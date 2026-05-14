61acbabd92ff8f5e5c45349ebcb176765c63c521
bxgda <jovan.bogdanovic@elfak.rs>
2026-05-14 23:19:59 +0200

    feat(models)

---
+++ rest-service/Models/SensorReading.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace rest_service.Models;
+
+[Table("sensor_readings")]
+public class SensorReading
+{
+    [Key]
+    [Column("id")]
+    public long Id { get; set; }
+
+    [Required]
+    [Column("ts")]
+    public DateTime Ts { get; set; }
+
+    [Required]
+    [Column("device_id")]
+    [MaxLength(20)]
+    public string DeviceId { get; set; } = string.Empty;
+
+    [Column("co")]
+    public double? Co { get; set; }
+
+    [Column("humidity")]
+    public double? Humidity { get; set; }
+
+    [Column("light")]
+    public bool? Light { get; set; }
+
+    [Column("lpg")]
+    public double? Lpg { get; set; }
+
+    [Column("motion")]
+    public bool? Motion { get; set; }
+
+    [Column("smoke")]
+    public double? Smoke { get; set; }
+
+    [Column("temp")]
+    public double? Temp { get; set; }
+}
